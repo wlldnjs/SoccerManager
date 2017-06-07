e557aa2d4767a574b51b1f826383746dd20ff010
jiwon <wlldnjs@naver.com>
2017-06-07 13:52:23 +0900

    테스트2

--- app/src/main/java/com/jiwonkim/soccermanager/MainActivity.java
+++ app/src/main/java/com/jiwonkim/soccermanager/MainActivity.java
@@ -2,9 +2,10 @@ package com.jiwonkim.soccermanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView = (TextView)findViewById(R.id.text);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
